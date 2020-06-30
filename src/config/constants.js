eafa74d8532e5556262fb7cc3f5bf9ee42e58f66
paula-morales <pmpoclava@gmail.com>
2020-06-30 22:38:11 +0200

    set dot env

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl = process.env.API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-export const apiKeyGoogle = "AIzaSyCWY428yUU937BQIVJdeSlQA-j-0X0yGMQ";
+export const apiKeyGoogle = process.env.REACT_APP_GOOGLE_API;
