3689b93571c68583f3e64bdcdbe8b208d027561e
paula-morales <pmpoclava@gmail.com>
2020-07-01 21:44:39 +0200

    Links added to navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,14 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectToken } from "../../store/user/selectors";
+import { selectToken, selectUser } from "../../store/user/selectors";
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 
 export default function Navigation() {
   const token = useSelector(selectToken);
-
+  const user = useSelector(selectUser);
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
 
   return (
@@ -38,7 +38,15 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav" style={{ flexGrow: 0 }}>
         <Nav>
           {!token ? <NavbarItem path="/signup" linkText="Sign up" /> : null}
-
+          {user.isOwner ? (
+            <NavbarItem path="/registerpet" linkText="Register your pet" />
+          ) : null}
+          {user.isCandidate ? (
+            <NavbarItem
+              path="/registerservice"
+              linkText="Register your service"
+            />
+          ) : null}
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
