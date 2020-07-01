3cb4518acf95c370caa5d0a88896acea3aa86466
paula-morales <pmpoclava@gmail.com>
2020-07-01 19:37:38 +0200

    form to create a new user

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -3,46 +3,166 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { signUp } from "../../store/user/actions";
-import { selectToken } from "../../store/user/selectors";
+import { selectUser } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import Geocode from "react-geocode";
+import { apiKeyGoogle } from "../../config/constants";
+import { showMessageWithTimeout } from "../../store/appState/actions";
 
 export default function SignUp() {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
   const dispatch = useDispatch();
-  const token = useSelector(selectToken);
+  const user = useSelector(selectUser);
   const history = useHistory();
+  const [radioValue, setRadioValue] = useState("");
+
+  const radios = [
+    { name: "I am a pet owner", value: "1" },
+    { name: "I want to offer pet services", value: "2" },
+  ];
 
   useEffect(() => {
-    if (token !== null) {
-      history.push("/");
+    if (user.isOwner) {
+      history.push("/registeryourpet");
+    } else if (user.isCandidate) {
+      history.push("/registeryourservice");
     }
-  }, [token, history]);
+  }, [user, history]);
 
+  Geocode.setApiKey(apiKeyGoogle);
+  Geocode.setRegion("nl");
+
+  let isOwner = false;
+  let isCandidate = false;
   function submitForm(event) {
     event.preventDefault();
+    if (radioValue === "1") {
+      isOwner = true;
+      isCandidate = false;
+    } else if (radioValue === "2") {
+      isOwner = false;
+      isCandidate = true;
+    }
+    if (
+      !firstName ||
+      !lastName ||
+      !phoneNumber ||
+      !email ||
+      !password ||
+      !address ||
+      (!isOwner && !isCandidate) ||
+      (isOwner && isCandidate)
+    ) {
+      dispatch(
+        showMessageWithTimeout("danger", true, "Please fill out all the fields")
+      );
+    } else {
+      Geocode.fromAddress(address).then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
 
-    dispatch(signUp(name, email, password));
+          dispatch(
+            signUp(
+              firstName,
+              lastName,
+              profilePicture,
+              phoneNumber,
+              lat,
+              lng,
+              email,
+              password,
+              isOwner,
+              isCandidate
+            )
+          );
+        },
+        (error) => {
+          dispatch(
+            showMessageWithTimeout(
+              "danger",
+              true,
+              "Sorry, we did not find the address"
+            )
+          );
+        }
+      );
+    }
 
     setEmail("");
     setPassword("");
-    setName("");
+    setFirstName("");
+    setLastName("");
+    setPhoneNumber("");
+    setAddress("");
+    setRadioValue("");
+    setProfilePicture("");
   }
 
   return (
     <Container>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-        <h1 className="mt-5 mb-5">Signup</h1>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-4">
+        <h4 className="mt-2 mb-4">
+          <strong>Sign up</strong>
+        </h4>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formBasicFirstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              type="text"
+              placeholder="Enter first name"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formBasicLastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              type="text"
+              placeholder="Enter last name"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formBasicPhone">
+            <Form.Label>Phone number</Form.Label>
+            <Form.Control
+              value={phoneNumber}
+              onChange={(event) => setPhoneNumber(event.target.value)}
+              type="number"
+              placeholder="Enter phone number"
+              required
+            />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formBasicPicture">
+            <Form.Label>Profile picture</Form.Label>
+            <Form.Control
+              value={profilePicture}
+              onChange={(event) => setProfilePicture(event.target.value)}
+              type="text"
+              placeholder="Enter picture url"
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+        <Form.Group controlId="formBasicAddress">
+          <Form.Label>Address</Form.Label>
           <Form.Control
-            value={name}
-            onChange={event => setName(event.target.value)}
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
             type="text"
-            placeholder="Enter name"
+            placeholder="Enter address"
             required
           />
         </Form.Group>
@@ -50,7 +170,7 @@ export default function SignUp() {
           <Form.Label>Email address</Form.Label>
           <Form.Control
             value={email}
-            onChange={event => setEmail(event.target.value)}
+            onChange={(event) => setEmail(event.target.value)}
             type="email"
             placeholder="Enter email"
             required
@@ -59,23 +179,51 @@ export default function SignUp() {
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
             value={password}
-            onChange={event => setPassword(event.target.value)}
+            onChange={(event) => setPassword(event.target.value)}
             type="password"
             placeholder="Password"
             required
           />
         </Form.Group>
-        <Form.Group className="mt-5">
+        <fieldset>
+          <Form.Group as={Row}>
+            <Col sm={10}>
+              {radios.map((radio, idx) => (
+                <Form.Check
+                  key={idx}
+                  type="radio"
+                  variant="primary"
+                  value={radio.value}
+                  checked={radioValue === radio.value}
+                  onChange={(e) => {
+                    setRadioValue(e.currentTarget.value);
+                  }}
+                  label={radio.name}
+                  name="formHorizontalRadios"
+                  id={`formHorizontalRadios${radio.value}`}
+                />
+              ))}
+            </Col>
+          </Form.Group>
+        </fieldset>
+
+        <Form.Group controlId="formBasicCheckbox"></Form.Group>
+        <Form.Group className="mt-0">
           <Button variant="primary" type="submit" onClick={submitForm}>
             Sign up
           </Button>
         </Form.Group>
-        <Link to="/login">Click here to log in</Link>
+        <label>Already have an account?</label>
+        <Link
+          to="/login"
+          style={{ paddingBottom: "5rem", marginLeft: "0.5rem" }}
+        >
+          Log in
+        </Link>
       </Form>
     </Container>
   );
