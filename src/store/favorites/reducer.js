0a64b1a2ef4abed57b15bed0f4b047b223e8eb0f
paula-morales <pmpoclava@gmail.com>
2020-07-04 12:42:57 +0200

    reducer cases ADD/REMOVE favorites

--- src/store/favorites/reducer.js
+++ src/store/favorites/reducer.js
@@ -5,6 +5,12 @@ export default (state = initialState, { type, payload }) => {
     case "favorites_fetched": {
       return payload;
     }
+    case "add_favorite": {
+      return [...state, ...payload];
+    }
+    case "remove_favorite": {
+      return [...state.filter((favorite) => favorite.id !== payload)];
+    }
     default:
       return state;
   }
